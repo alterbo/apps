1c2b6ed56307167cc5f7edd23761f733632b5c1a
Alberto Ferrero <albertonomo@gmail.com>
2026-07-19 15:23:56 +0200

    fix build for illuminati app update netlify toml files

--- apps/illuminati/svelte.config.js
+++ apps/illuminati/svelte.config.js
@@ -1,15 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+
+/**
+ * @param {{ filename: string }} options
+ * @returns {boolean | undefined}
+ */
+function runesMode({ filename }) {
+	return filename.split(/[/\\]/).includes('node_modules') ? undefined : true;
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+		runes: runesMode
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
